16f307d33951988b930df1a80b3cec8dd657fd91
sachinit254 <sachinit254@gmail.com>
2021-12-01 15:30:31 +0530

    Portfolio Website

---
+++ index.js
@@ -0,0 +1,21 @@
+const menuBtn = document.querySelector(".navToggler");
+const collapsedNav = document.querySelector(".menu")
+menuBtn.addEventListener("click", () => {
+  collapsedNav.classList.toggle("hidden")
+});
+
+
+function updateList() {
+	const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
+		return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+	});
+
+	document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+	
+	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+}
+
+updateList();
+window.addEventListener('scroll', () => {
+    updateList();
+})
